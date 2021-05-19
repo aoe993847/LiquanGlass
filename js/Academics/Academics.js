fe5fa12a30ddb1015a57aebf189f7b002c058fc8
yao <aoe553@gmail.com>
2021-05-19 13:22:26 +0800

    .

--- js/Academics/Academics.js
+++ js/Academics/Academics.js
@@ -1,3 +1,33 @@
+;(function($){
+    "use strict"
+	
+	
+	var nav_offset_top = $('header').height() + 50; 
+    /*-------------------------------------------------------------------------------
+	  Navbar 
+	-------------------------------------------------------------------------------*/
+
+	//* Navbar Fixed  
+    function navbarFixed(){
+        if ( $('.header_area').length ){ 
+            $(window).scroll(function() {
+                var scroll = $(window).scrollTop();   
+                if (scroll >= nav_offset_top ) {
+                    $(".header_area").addClass("navbar_fixed");
+                } else {
+                    $(".header_area").removeClass("navbar_fixed");
+                }
+            });
+        };
+    };
+    navbarFixed();
+	
+
+	
+
+})(jQuery)
+
+
 $(document).ready(function() { 
     var a = $("#pagelink").offset().top; 
 
@@ -30,89 +60,5 @@ $(".paragraph_D").click(function(){
 $(".paragraph_E").click(function(){
     $(".paragraph_e").slideToggle();
 });
-	/*------------------
-		Background Set
-	--------------------*/
-	$('.set-bg').each(function () {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
 
 
-    (function ($) {
-
-        /*------------------
-            Background Set
-        --------------------*/
-        $('.set-bg').each(function () {
-            var bg = $(this).data('setbg');
-            $(this).css('background-image', 'url(' + bg + ')');
-        });
-    
-    
-        /*------------------
-            Hero Slider
-        --------------------*/
-        var sliderCount;
-        $(".hero-slider").on("initialized.owl.carousel", function (e) {
-            sliderCount = e.item.count;
-            if (sliderCount < 10) {
-                sliderCount = "0" + sliderCount;
-            }
-        }).owlCarousel({
-            loop: true,
-            margin: 0,
-            nav: true,
-            items: 1,
-            dots: false,
-            animateOut: 'fadeOut',
-            animateIn: 'fadeIn',
-            navText: ['<img src="img/icons/arrow-left-white.png" alt="">', '<img src="img/icons/arrow-right-black.png" alt="">'],
-            smartSpeed: 1200,
-            autoplay: true,
-            mouseDrag: false
-        }).on("changed.owl.carousel", function (e) {
-    
-            var Index = e.item.index - 1;
-            var Count = e.item.count;
-            var PreIndex = Index - 1;
-            var NextIndex = Index + 1;
-    
-    
-            if (PreIndex < 0) {
-                PreIndex = Count - 1;
-            }
-    
-            if (PreIndex < 1) {
-                PreIndex = Count;
-            }
-    
-            if (PreIndex < 10) {
-                PreIndex = "0" + PreIndex;
-            }
-    
-            if (NextIndex > Count) {
-                NextIndex = 1;
-            }
-    
-            if (NextIndex < 10) {
-                NextIndex = "0" + NextIndex;
-            }
-    
-    
-            $(".hero-slider .owl-nav button.owl-prev").html('<img src="img/icons/arrow-left-white.png" alt=""> <span> ' + PreIndex + '</span> ');
-            $(".hero-slider .owl-nav button.owl-next").html('<span> ' + NextIndex + ' </span> <img src="img/icons/arrow-right-black.png" alt="">');
-        });
-    
-        $(".hero-slider .owl-nav button.owl-prev").html('<img src="img/icons/arrow-left-white.png" alt=""> <span> ' + sliderCount + '</span> ');
-    
-        $(".hero-slider .owl-nav button.owl-next").html('<span>02</span> <img src="img/icons/arrow-right-black.png" alt="">');
-    
-    
-    
-    })(jQuery);
-    
-    
-    /*------------------
-        Features Slider
-    --------------------*/
