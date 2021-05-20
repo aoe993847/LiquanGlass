3983a436183132e28de9ed423b8118598fcd5a29
yao <aoe553@gmail.com>
2021-05-20 14:43:01 +0800

    .

--- js/Academics/Academics.js
+++ js/Academics/Academics.js
@@ -28,21 +28,6 @@
 })(jQuery)
 
 
-$(document).ready(function() { 
-    var a = $("#pagelink").offset().top; 
-
-    $(document).scroll(function(){ 
-     if($(this).scrollTop() > 150) 
-     { 
-        $('#pagelink').addClass("navbar-fixed-top");; 
-      $('#pagelink').css({"background":"rgba(0, 0, 0, 0.541)"}); 
-     } else if($(this).scrollTop() < 50){ 
-      $('#pagelink').css({"background":"black"}); 
-      $('#pagelink').removeClass("navbar-fixed-top");;
-     } 
-    }); 
-   }); 
-
 
 
 $(".paragraph_A").click(function(){
