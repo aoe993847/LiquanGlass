dd9bdec88d6d78f7e3f73c298b986552dc63e810
wqsaxzedc5 <wqsaxzedc5@gmail.com>
2021-08-04 14:29:37 +0800

    變

--- js/products/product_content.js
+++ js/products/product_content.js
@@ -1837,6 +1837,227 @@ $(document).ready(function () {
         }
         }
         //-->
+        //==================================== 頁面切換 =============================
+        // 換
+(function($){
+    
+  var paginate = {
+      startPos: function(pageNumber, perPage) {
+          // determine what array position to start from
+          // based on current page and # per page
+          return pageNumber * perPage;
+      },
+
+      getPage: function(items, startPos, perPage) {
+          // declare an empty array to hold our page items
+          var page = [];
+
+          // only get items after the starting position
+          items = items.slice(startPos, items.length);
+
+          // loop remaining items until max per page
+          for (var i=0; i < perPage; i++) {
+              page.push(items[i]); }
+
+          return page;
+      },
+
+      totalPages: function(items, perPage) {
+          // determine total number of pages
+          return Math.ceil(items.length / perPage);
+      },
+
+      createBtns: function(totalPages, currentPage) {
+          // create buttons to manipulate current page
+          var pagination = $('<footer class="pagination" />');
+
+          // add a "first" button
+          pagination.append('<span class="pagination-button">&laquo;</span>');
+
+          // add pages inbetween
+          for (var i=1; i <= totalPages; i++) {
+              // truncate list when too large
+              if (totalPages > 5 && currentPage !== i) {
+                  // if on first two pages
+                  if (currentPage === 1 || currentPage === 2) {
+                      // show first 5 pages
+                      if (i > 5) continue;
+                  // if on last two pages
+                  } else if (currentPage === totalPages || currentPage === totalPages - 1) {
+                      // show last 5 pages
+                      if (i < totalPages - 4) continue;
+                  // otherwise show 5 pages w/ current in middle
+                  } else {
+                      if (i < currentPage - 2 || i > currentPage + 2) {
+                          continue; }
+                  }
+              }
+
+              // markup for page button
+              var pageBtn = $('<span class="pagination-button page-num" />');
+
+              // add active class for current page
+              if (i == currentPage) {
+                  pageBtn.addClass('active'); }
+
+              // set text to the page number
+              pageBtn.text(i);
+
+              // add button to the container
+              pagination.append(pageBtn);
+          }
+
+          // add a "last" button
+          pagination.append($('<span class="pagination-button">&raquo;</span>'));
+
+          return pagination;
+      },
+
+      createPage: function(items, currentPage, perPage) {
+          // remove pagination from the page
+          $('.pagination').remove();
+
+          // set context for the items
+          var container = items.parent(),
+              // detach items from the page and cast as array
+              items = items.detach().toArray(),
+              // get start position and select items for page
+              startPos = this.startPos(currentPage - 1, perPage),
+              page = this.getPage(items, startPos, perPage);
+
+          // loop items and readd to page
+          $.each(page, function(){
+              // prevent empty items that return as Window
+              if (this.window === undefined) {
+                  container.append($(this)); }
+          });
+
+          // prep pagination buttons and add to page
+          var totalPages = this.totalPages(items, perPage),
+              pageButtons = this.createBtns(totalPages, currentPage);
+
+          container.after(pageButtons);
+      }
+  };
+
+  // stuff it all into a jQuery method!
+  $.fn.paginate = function(perPage) {
+      var items = $(this);
+
+      // default perPage to 5
+      if (isNaN(perPage) || perPage === undefined) {
+          perPage = 5; }
+
+      // don't fire if fewer items than perPage
+      if (items.length <= perPage) {
+          return true; }
+
+      // ensure items stay in the same DOM position
+      if (items.length !== items.parent()[0].children.length) {
+          items.wrapAll('<div class="pagination-items" />');
+      }
+
+      // paginate the items starting at page 1
+      paginate.createPage(items, 1, perPage);
+
+      // handle click events on the buttons
+      $(document).on('click', '.pagination-button', function(e) {
+          // get current page from active button
+          var currentPage = parseInt($('.pagination-button.active').text(), 10),
+              newPage = currentPage,
+              totalPages = paginate.totalPages(items, perPage),
+              target = $(e.target);
+
+          // get numbered page
+          newPage = parseInt(target.text(), 10);
+          if (target.text() == '«') newPage = 1;
+          if (target.text() == '»') newPage = totalPages;
+
+          // ensure newPage is in available range
+          if (newPage > 0 && newPage <= totalPages) {
+              paginate.createPage(items, newPage, perPage); }
+      });
+  };
+
+})(jQuery);
+
+
+
+$('.list-1').paginate(15);
+// 隱藏1
+function listBtn() {
+  var textlistn = document.getElementById('textlistn');
+  if (textlistn.style.display === 'none') {
+    textlistn.style.display = 'block';
+  textlistn1.style.display = 'none';
+  textlistn2.style.display = 'none';
+  textlistn3.style.display = 'none';
+ textlistn4.style.display = 'none';
+   textlistn5.style.display = 'none';
+  } 
+}
+// 隱藏2
+function listBtn1() {
+  var textlistn1 = document.getElementById('textlistn1');
+  if (textlistn1.style.display === 'none') {
+    textlistn1.style.display = 'block';
+    textlistn.style.display = 'none';
+    textlistn2.style.display = 'none';
+    textlistn3.style.display = 'none';
+    textlistn4.style.display = 'none';
+    textlistn5.style.display = 'none';
+  } 
+}
+// 隱藏3
+function listBtn2() {
+  var textlistn2 = document.getElementById('textlistn2');
+  if (textlistn2.style.display === 'none') {
+    textlistn2.style.display = 'block';
+    textlistn.style.display = 'none';
+    textlistn1.style.display = 'none';
+    textlistn3.style.display = 'none';
+    textlistn4.style.display = 'none';
+    textlistn5.style.display = 'none';
+  } 
+}
+// 隱藏4
+function listBtn3() {
+  var textlistn3 = document.getElementById('textlistn3');
+  if (textlistn3.style.display === 'none') {
+    textlistn3.style.display = 'block';
+    textlistn.style.display = 'none';
+    textlistn1.style.display = 'none';
+    textlistn2.style.display = 'none';
+    textlistn4.style.display = 'none';
+    textlistn5.style.display = 'none';
+  } 
+}
+// 隱藏5
+function listBtn4() {
+  var textlistn4 = document.getElementById('textlistn4');
+  if (textlistn4.style.display === 'none') {
+    textlistn4.style.display = 'block';
+    textlistn.style.display = 'none';
+    textlistn1.style.display = 'none';
+    textlistn3.style.display = 'none';
+    textlistn2.style.display = 'none';
+    textlistn5.style.display = 'none';
+  } 
+}
+// 隱藏6
+function listBtn5() {
+  var textlistn5 = document.getElementById('textlistn5');
+  if (textlistn5.style.display === 'none') {
+    textlistn5.style.display = 'block';
+    textlistn.style.display = 'none';
+    textlistn1.style.display = 'none';
+    textlistn2.style.display = 'none';
+    textlistn4.style.display = 'none';
+    textlistn3.style.display = 'none';
+  } 
+}
+
+
 
   
 
